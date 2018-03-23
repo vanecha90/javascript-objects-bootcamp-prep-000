110afe2df205d3693184fa52bb40ae63e33d5d93
vanessa.collado90@gmail.com <vanessa.collado90@gmail.com>
2018-03-23 05:56:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,6 +9,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
- playlist[artistName] = songTitle
+ delete playlist[artistName] 
  return playlist
 }
